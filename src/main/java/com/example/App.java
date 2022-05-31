f3ca7a88df7636e5ba7ffe9b9b7af947ed6e915b
dianaagustinaf <dianaagustinaf@gmail.com>
2022-05-31 10:39:50 +0200

    map-filter

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -45,6 +47,33 @@ public class App
         for (Persona persona : mujeres) {
             System.out.println(persona);
         }
+
+        System.out.println("________________________________________");
+
+
+
+        Map<Persona.Sexo, List<Persona>> personasPorSexo = new HashMap<>();
+
+        personasPorSexo = personas.stream()
+                .collect(Collectors.groupingBy(Persona::getSexo));
+        
+        // para algun proceso adicional, recorrer con loop sino no hace falta
+        for (Map.Entry<Persona.Sexo, List<Persona>> entry : personasPorSexo.entrySet()) {
+           
+            Persona.Sexo key = entry.getKey();
+
+            System.out.println("Personas del sexo " + key);
+
+            List<Persona> pPorSexo = entry.getValue();
+
+            pPorSexo.stream().forEach(p-> System.out.println(p.getNombre()));
+
+           //System.out.println(entry);
+           System.out.println("________________________________________");
+        }
+
+        System.out.println("--------------- Imprimir Directamente -----------------------");
+        System.out.println(personasPorSexo);
 
     }
 }
